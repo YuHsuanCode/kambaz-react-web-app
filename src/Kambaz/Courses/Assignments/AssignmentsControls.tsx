b52af80f67deea67668c711cc6fbee77ff49af7d
YuHsuan(Krystal)Yang <yuh2636@gmail.com>
2026-03-28 00:59:00 -0400

    fix build

--- src/Kambaz/Courses/Assignments/AssignmentsControls.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentsControls.tsx
@@ -1,74 +1,77 @@
-//add new assignments
+// add new assignments
 import AssignmentsEditor from "./AssignmentsEditor";
-import {BsSearch} from "react-icons/bs";
+import { BsSearch } from "react-icons/bs";
 
 interface AssignmentsControlsProps {
-    dialogTitle: string;
-    assignmentName: {
-        title: string;
-        description: string;
-        points: string;
-        dueDate: string;
-        availableFrom: string;
-        availableTo: string;
-        _id?: string;
-    };
-    setAssignmentName: (assignment: any) => void;
-    addAssignment: () => void;
-    saveAssignment: () => void;
-    isEditing?: boolean;
+  dialogTitle: string;
+  assignmentName: {
+    title: string;
+    description: string;
+    points: string;
+    dueDate: string;
+    availableFrom: string;
+    availableTo: string;
+    _id?: string;
+  };
+  setAssignmentName: (assignment: any) => void;
+  addAssignment: () => void;
+  saveAssignment: () => void;
+  isEditing?: boolean;
 }
 
 export default function AssignmentsControls({
-    dialogTitle,
-    assignmentName,
-    setAssignmentName,
-    addAssignment,
-    saveAssignment,
-    isEditing = false
+  dialogTitle,
+  assignmentName,
+  setAssignmentName,
+  addAssignment,
+  saveAssignment,
+  isEditing = false,
 }: AssignmentsControlsProps) {
-    return (
-        <div className="d-flex justify-content-between align-items-center">
-            <div className="input-group w-50">
-                <span className="input-group-text">
-                    <BsSearch/>  
-                </span>
-                <input 
-                    type="text" 
-                    placeholder="Search..." 
-                    id="wd-search-assignment" 
-                    className="form-control w-50"
-                />
-            </div>
+  return (
+    <div className="d-flex justify-content-between align-items-center">
+      {/* Search */}
+      <div className="input-group w-50">
+        <span className="input-group-text">
+          <BsSearch />
+        </span>
+        <input
+          type="text"
+          placeholder="Search..."
+          id="wd-search-assignment"
+          className="form-control"
+        />
+      </div>
 
-            <div className="d-flex">
-                <button 
-                    id="wd-add-assignment-group" 
-                    className="btn btn-light me-2"
-                    data-bs-toggle="modal"
-                    data-bs-target="#wd-add-assignment-dialog"
-                >
-                    + Group
-                </button>
+      {/* Buttons */}
+      <div className="d-flex">
+        <button
+          id="wd-add-assignment-group"
+          className="btn btn-light me-2"
+          data-bs-toggle="modal"
+          data-bs-target="#wd-add-assignment-dialog"
+        >
+          + Group
+        </button>
 
-                <button 
-                    id="wd-add-assignment" 
-                    className="btn btn-danger me-1"
-                    data-bs-toggle="modal"
-                    data-bs-target="#wd-add-assignment-dialog"
-                >
-                    + Assignment
-                </button>
-            </div>
+        <button
+          id="wd-add-assignment"
+          className="btn btn-danger me-1"
+          data-bs-toggle="modal"
+          data-bs-target="#wd-add-assignment-dialog"
+        >
+          + Assignment
+        </button>
+      </div>
 
-            <AssignmentsEditor 
-                dialogTitle={dialogTitle}
-                assignmentName={assignmentName}
-                setAssignmentName={setAssignmentName}
-                addAssignment={addAssignment}
-                saveAssignment={saveAssignment}
-                isEditing={isEditing}
-            />
-        </div>
-    );
+      {/* Modal Editor */}
+      <AssignmentsEditor
+        dialogTitle={dialogTitle}
+        assignmentName={assignmentName}
+        setAssignmentName={setAssignmentName}
+        addAssignment={addAssignment}
+        saveAssignment={saveAssignment}
+        isEditing={isEditing}
+      />
+    </div>
+  );
 }
--- src/Kambaz/Courses/Assignments/AssignmentsEditor.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentsEditor.tsx
@@ -1,84 +1,106 @@
 /*pop up when you click  +Assignment buttons a new assignment editor*/
 //import {Link} from "react-router-dom";
-
-export default function AssignmentsEditor({ dialogTitle, assignmentName, setAssignmentName, addAssignment,
-    saveAssignment
-}:
-    {dialogTitle: string; 
-    assignmentName:{
-        title: string;
-        description: string;
-        points: string;
-        dueDate: string;
-        availableFrom: string;
-        availableTo: string;
-     };
-    setAssignmentName:(assignment:any) => void;
-    addAssignment: () => void;
-    saveAssignment: () => void;
-
-    }){
-        return(
-            <div id="wd-add-assignment-dialog" className="modal fade" data-bs-backdrop="static" data-bs-keyboard="false">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="staticBackdropLabel">
-                                {dialogTitle}
-                            </h1>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
-                        </div>
-                        
-                        <div className="modal-body">
-
-                        <input className="form-control mb-2" 
-                                value={assignmentName.title} placeholder="New Assignment"
-                                onChange={(e) => setAssignmentName({...assignmentName, title: e.target.value})}/>
-
-                        <textarea className="form-control mb-2" 
-                                value={assignmentName.description} placeholder="New Assignment Description"
-                                onChange={(e) => setAssignmentName({...assignmentName, description: e.target.value})}/>
-
-
-                        <label htmlFor="wd-points" className="wd-grid-col-two-thirds-page">Points</label>
-                        <input id="wd-points" className="form-control" value={assignmentName.points} placeholder="Assignement Points"
-                                onChange={(e) => setAssignmentName({...assignmentName, points: e.target.value})}/>
-                        </div>
-                    
-                        <div className="flex-container-assign wd-grid-col-third-page">
-                            
-                            <label htmlFor="wd-assign-to" className="p-2 fw-bold">Assign to</label>
-
-                            <input id="wd-assign-to" type="text" defaultValue="Everyone" className="form-control"/>
-                                <label htmlFor="wd-due-date" className="p-2 fw-bold">Due</label><br/>
-                                <input id="wd-due-date" className="form-control" value={assignmentName.dueDate}
-                                    type="date"
-                                    onChange={(e) => setAssignmentName({...assignmentName, dueDate: e.target.value})}/>
-
-                            <label htmlFor="wd-available-from" className="wd-grid-col-half-page p-2 fw-bold">Available From</label>
-                            <label htmlFor="wd-available-until" className="p-2 fw-bold">Until</label>
-
-                                <input type="date" id="wd-available-from" className="wd-grid-col-half-page form-control" value={assignmentName.availableFrom}
-                                onChange={(e) => setAssignmentName({...assignmentName, availableFrom: e.target.value})}/>
-                                
-                                <input type="date" id="wd-available-until" value={assignmentName.availableTo} className="wd-grid-col-half-page form-control"
-                                onChange={(e) => setAssignmentName({...assignmentName, availableTo: e.target.value})}/>
-                        </div>
-
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" 
-                                data-bs-dismiss="modal">Cancel</button>
-                            <button onClick={addAssignment} type="button" 
-                                data-bs-dismiss="modal" className="btn btn-danger">Add</button>
-                            <button onClick={saveAssignment} type="button" 
-                                data-bs-dismiss="modal" className="btn btn-danger">Save Assignment</button>
-                        </div>
-                        
-                       
-                    </div>
-                </div>
+export default function AssignmentsEditor({
+    dialogTitle,
+    assignmentName,
+    setAssignmentName,
+    addAssignment,
+    saveAssignment,
+    isEditing = false,
+  }: any) {
+    return (
+      <div
+        id="wd-add-assignment-dialog"
+        className="modal fade"
+        data-bs-backdrop="static"
+        data-bs-keyboard="false"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+  
+            <div className="modal-header">
+              <h1 className="modal-title fs-5">{dialogTitle}</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal"></button>
             </div>
-        );
-    
-}
+  
+            <div className="modal-body">
+  
+              <input
+                className="form-control mb-2"
+                value={assignmentName.title || ""}
+                placeholder="New Assignment"
+                onChange={(e) =>
+                  setAssignmentName({
+                    ...assignmentName,
+                    title: e.target.value,
+                  })
+                }
+              />
+  
+              <textarea
+                className="form-control mb-2"
+                value={assignmentName.description || ""}
+                placeholder="Description"
+                onChange={(e) =>
+                  setAssignmentName({
+                    ...assignmentName,
+                    description: e.target.value,
+                  })
+                }
+              />
+  
+              <input
+                className="form-control mb-2"
+                value={assignmentName.points || ""}
+                placeholder="Points"
+                onChange={(e) =>
+                  setAssignmentName({
+                    ...assignmentName,
+                    points: e.target.value,
+                  })
+                }
+              />
+  
+              <input
+                type="date"
+                className="form-control mb-2"
+                value={assignmentName.dueDate || ""}
+                onChange={(e) =>
+                  setAssignmentName({
+                    ...assignmentName,
+                    dueDate: e.target.value,
+                  })
+                }
+              />
+  
+            </div>
+  
+            <div className="modal-footer">
+              <button className="btn btn-secondary" data-bs-dismiss="modal">
+                Cancel
+              </button>
+  
+              {isEditing ? (
+                <button
+                  onClick={saveAssignment}
+                  data-bs-dismiss="modal"
+                  className="btn btn-success"
+                >
+                  Save
+                </button>
+              ) : (
+                <button
+                  onClick={addAssignment}
+                  data-bs-dismiss="modal"
+                  className="btn btn-danger"
+                >
+                  Add
+                </button>
+              )}
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    );
+  }
